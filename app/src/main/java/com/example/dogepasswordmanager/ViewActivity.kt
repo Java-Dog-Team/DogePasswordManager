cbd4e8cfdbac1861ef02dbb7e4826b517687fd17
Mosmochi <sw710407@gmail.com>
2024-04-27 21:08:23 +0800

    feat: 編輯、刪除按鈕

--- app/src/main/java/com/example/dogepasswordmanager/ViewActivity.kt
+++ app/src/main/java/com/example/dogepasswordmanager/ViewActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dogepasswordmanager
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -14,27 +15,44 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ClipboardManager
 import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.example.dogepasswordmanager.ui.theme.BrickRed
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 class ViewActivity : ComponentActivity() {
 
@@ -48,15 +66,62 @@ class ViewActivity : ComponentActivity() {
         }
     }
 }
-
 private lateinit var intentObj: Intent
-
+//firebase 資料庫
+private val db = Firebase.firestore
 @Composable
 fun ViewPage(context: Context) {
-
+    var openDialog by remember {
+        mutableStateOf(false)
+    }
+    val activity = LocalContext.current as Activity
     //剪貼簿管理員物件
     val clipboardManager: ClipboardManager = LocalClipboardManager.current
-    Row(modifier = Modifier.fillMaxSize()) {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+
+        if (openDialog) {
+            AlertDialog(
+                icon = {
+                    Icon(Icons.Filled.Warning, contentDescription = "Example Icon")
+                },
+                title = {
+                    Text(text = "刪除項目", fontSize = 30.sp)
+                },
+                text = {
+                    Text(text = "您確定要刪除此項目嗎?", fontSize = 20.sp)
+                },
+                onDismissRequest = {
+                    openDialog = false
+                },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            //按下確認刪除後的操作
+                            openDialog = false
+
+                            //刪除檔案
+                        }
+                    ) {
+                        Text("確定")
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            //按下取消刪除後的操作
+                            openDialog = false
+                        }
+                    ) {
+                        Text("取消")
+                    }
+                }
+            )
+        }
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -238,7 +303,10 @@ fun ViewPage(context: Context) {
 
                     //刪除按鈕
                     FloatingActionButton(
-                        onClick = { /*TODO*/ },
+                        onClick = {
+                            //按下刪除按鈕後的操作
+                            openDialog = true
+                        },
                         shape = CircleShape,
                         containerColor = BrickRed,
                         contentColor = Color.White,
@@ -254,3 +322,5 @@ fun ViewPage(context: Context) {
 
     }
 }
+
+
